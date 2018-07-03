c34aa70a4b78c95f76b5735095d78743cba88177
Stephen <stephen@whitebolt.net>
2018-07-03 05:10:42 +0100

    Refactor to ES6 and change style
    Changed the style to use tabs instead of spaces (we prefer this, no reason to love one and hate the other).  Refactored everything to async/await and moved code into separate functions.  Some more refactoring needed to simplify each function and shorten them (ie. more but shorter functions).

--- index.js
+++ index.js
@@ -1,86 +1,103 @@
-var Readable = require('stream').Readable;
-var path = require('path');
+const Readable = require('stream').Readable;
+const path = require('path');
+const isString = require('lodash.isstring');
+const isObject = require('lodash.isobject');
 
-module.exports = function rollupStream(options) {
-  var stream = new Readable();
-  stream._read = function() {  };
-  
-  if(typeof options === 'object' && options !== null) {
-    options = Promise.resolve(options);
-  } else if(typeof options === 'string') {
-    var optionsPath = path.resolve(options);
-    options = require('rollup').rollup({
-      input: optionsPath,
-      onwarn: function(warning) {
-        if(warning.code !== 'UNRESOLVED_IMPORT') {
-          console.warn(warning.message);
-        }
-      }
-    }).then(function(bundle) {
-      return bundle.generate({ format: 'cjs' });
-    }).then(function(result) {
-      // don't look at me. this is how Rollup does it.
-      var defaultLoader = require.extensions['.js'];
-      
-      require.extensions['.js'] = function(m, filename) {
-        if(filename === optionsPath) {
-          m._compile(result.code, filename);
-        } else {
-          defaultLoader(m, filename);
-        }
-      };
-      
-      try {
-        return require(optionsPath);
-      } finally {
-        require.extensions['.js'] = defaultLoader;
-      }
-    });
-  } else {
-    options = Promise.reject(Error("options must be an object or a string!"));
-  }
-  
-  options.then(function(options0) {
-    var rollup = options0.rollup;
-    var hasCustomRollup = true;
-    if(!rollup) {
-      rollup = require('rollup');
-      hasCustomRollup = false;
-    }
-    
-    var options = {};
-    for(var key in options0) {
-      if(key === 'sourceMap' && !hasCustomRollup) {
-        console.warn(
-          "The sourceMap option has been renamed to \"sourcemap\" " +
-          "(lowercase \"m\") in Rollup. The old form is now deprecated " +
-          "in rollup-stream."
-        );
-        options.sourcemap = options0.sourceMap;
-      } else if(key !== 'rollup') {
-        options[key] = options0[key];
-      }
-    }
-    
-    return rollup.rollup(options).then(function(bundle) {
-      stream.emit('bundle', bundle);
-      
-      return bundle.generate(options);
-    }).then(function(result) {
-      var code = result.code, map = result.map;
-      
-      stream.push(code);
-      if(options.sourcemap || options.sourceMap) {
-        stream.push('\n//# sourceMappingURL=');
-        stream.push(map.toUrl());
-      }
-      stream.push(null);
-    });
-  }).catch(function(reason) {
-    setImmediate(function() {
-      stream.emit('error', reason);
-    });
-  });
-  
-  return stream;
-};
+
+function _getOptions(options, stream) {
+	return _parseOptions(isString(options) ?
+		_getStringOptions(options, stream) :
+		(isObject(options) ?
+			Promise.resolve(options) :
+			Promise.reject(new Error("options must be an object or a string!"))
+		)
+	);
+}
+
+async function _getStringOptions(optionsPath, stream) {
+	try {
+		const input = path.resolve(optionsPath);
+
+		const bundle = await require('rollup').rollup({
+			input: input,
+			onwarn: warning=> {
+				if (warning.code !== 'UNRESOLVED_IMPORT') console.warn(warning.message);
+			}
+		});
+		const result = await bundle.generate({format: 'cjs'});
+
+		// don't look at me. this is how Rollup does it.
+		const defaultLoader = require.extensions['.js'];
+
+		require.extensions['.js'] = function(module, filename) {
+			if (filename === input) {
+				module._compile(result.code, filename);
+			} else {
+				defaultLoader(module, filename);
+			}
+		};
+
+		try {
+			return require(input);
+		} finally {
+			require.extensions['.js'] = defaultLoader;
+		}
+	} catch (err) {
+		setImmediate(()=>stream.emit('error', err));
+	}
+
+	return {};
+}
+
+async function _parseOptions(options, stream) {
+	try {
+		const _options = await options;
+		const rollup = _options.rollup || require('rollup');
+		const hasCustomRollup = !!_options.rollup;
+
+		const parsedOptions = {};
+		for (let key in _options) {
+			if (key === 'sourceMap' && !hasCustomRollup) {
+				console.warn(`The sourceMap option has been renamed to \"sourcemap\" (lowercase \"m\") in Rollup. The old form is now deprecated in rollup-stream.`);
+				parsedOptions.sourcemap = _options.sourceMap;
+			} else if (key !== 'rollup') {
+				parsedOptions[key] = _options[key];
+			}
+		}
+
+		return {options:parsedOptions, rollup};
+	} catch (err) {
+		setImmediate(()=>stream.emit('error', err));
+	}
+
+	return {};
+}
+
+async function go({options, rollup}, stream) {
+	try {
+		const bundle = await rollup.rollup(options);
+		stream.emit('bundle', bundle);
+		const {code, map} = await bundle.generate(options);
+
+		stream.push(code);
+		if(options.sourcemap || options.sourceMap) {
+			stream.push('\n//# sourceMappingURL=');
+			stream.push(map.toUrl());
+		}
+		stream.push(null);
+	} catch(err) {
+		setImmediate(()=>stream.emit('error', err));
+	}
+}
+
+function rollupStream(options) {
+	const stream = new Readable();
+	stream._read = function() {};
+
+	_getOptions(options, stream)
+		.then(options=>go(options, stream));
+
+	return stream;
+}
+
+module.exports = rollupStream;
